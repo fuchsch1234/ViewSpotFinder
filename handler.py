e5a482d31f9e0c9f45650b3a81aaf9b76f51e751
cf <christian+github@fuchsch.de>
2021-11-10 20:33:11 +0100

    Start viewspotfinder from serverless aws-python3 template

---
+++ handler.py
@@ -0,0 +1,14 @@
+import json
+
+
+def find_view_spot(mesh):
+    pass
+
+
+def handle_find_view_spot(event, context):
+    response = {
+        "statusCode": 200,
+        "body": json.dumps([])
+    }
+
+    return response
